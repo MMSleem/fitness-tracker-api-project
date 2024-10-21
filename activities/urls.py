4be601a628c82081d8118e714db06b3cfa7896b4
MMSleem <cptmsleem@gmail.com>
2024-10-21 20:21:16 +0300

    Modified Code for enhancement

--- activities/urls.py
+++ activities/urls.py
@@ -3,8 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import ActivityViewSet
 
 router = DefaultRouter()
-router.register(r'', ActivityViewSet, basename='activities')
+router.register(r'activities', ActivityViewSet, basename='activity')
 
 urlpatterns = [
-    path('activities/', include(router.urls)),
+    path('', include(router.urls)),
 ]
--- activities/views.py
+++ activities/views.py
@@ -2,13 +2,16 @@ from rest_framework import viewsets
 from .models import Activity
 from .serializers import ActivitySerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import filters
 
 class ActivityViewSet(viewsets.ModelViewSet):
     serializer_class = ActivitySerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['date', 'duration', 'calories_burned']
 
     def get_queryset(self):
-        return Activity.objects.filter(user=self.request.user)
+        return Activity.objects.select_related('user').filter(user=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
